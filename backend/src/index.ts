513ee89b13d67d4a4249a2f5b112cabf4c7a9fa7
Federico González <alambratore@hotmail.com>
2025-02-14 21:05:48 -0300

    Add public directory

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import { createServer } from 'http';
 import { initSocket } from './socket/socket.js';
+import { fileURLToPath } from 'url';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
@@ -13,6 +17,9 @@ const server = createServer(app);
 // Middlewares globales
 app.use(cors({ origin: '*' }));
 app.use(express.json());
+
+// Ruta para contenido estático
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Rutas de la aplicación. SIEMPRE DEBEN EMPEZAR POR "/api". HAY QUE MOVERLAS A OTROS ACHIVOS PORQUE ESTO VA A CRECER MUCHÍSIMO!
 app.get('/api/', (_req, res) => {
