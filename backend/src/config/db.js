54fd30af4d288ff5ae078c8ff669eb47bd2b35d8
Dmitri Jrapach <dmitrijrapach@gmail.com>
2025-02-17 20:43:45 -0300

    conectado al mongo

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,8 +6,8 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      ssl: true,
+      authSource: "admin",
     });
     console.log("🔥 MongoDB conectado");
   } catch (error) {
