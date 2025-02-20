88d0a00fe976eaa3d6eda84d95a35c8e7131114c
Dmitri Jrapach <dmitrijrapach@gmail.com>
2025-02-20 14:14:39 -0300

    sacando comentarios

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,24 @@
 import http from "http";
 import { Server } from "socket.io";
-import app from "./app.js"; // Importamos la configuración de Express
+import app from "./app.js"; 
 
 const PORT = process.env.PORT || 5000;
 
-// Creamos el servidor HTTP
 const server = http.createServer(app);
 
-// Configuración de Socket.io
 const io = new Server(server, {
   cors: {
-    origin: "*", // Puedes restringirlo a tu frontend
+    origin: "*", 
     methods: ["GET", "POST"]
   }
 });
 
-// Evento de conexión de socket
 io.on("connection", (socket) => {
   console.log(`🟢 Nuevo cliente conectado: ${socket.id}`);
 
   socket.on("message", (data) => {
     console.log("Mensaje recibido:", data);
-    io.emit("message", data); // Reenviar mensaje a todos los clientes conectados
+    io.emit("message", data); 
   });
 
   socket.on("disconnect", () => {
