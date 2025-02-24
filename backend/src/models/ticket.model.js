7f6c4a3501a148ea171d58ba717de7028c6d1faa
ezequielastrada <eastrada.in@gmail.com>
2025-02-24 20:53:40 -0300

    refactor ticket.model

--- backend/src/models/ticket.model.js
+++ backend/src/models/ticket.model.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
 export const ticketSchema = new mongoose.Schema({
-    user: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
-    trip: { type: mongoose.Types.ObjectId, ref: 'Trip', required: true },
+    user_id: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+    trip_id: { type: mongoose.Types.ObjectId, ref: 'Trip', required: true },
+    boarding_stop_id: {type: mongoose.Types.ObjectId, ref: 'TripStop', required: true},
+    alighting_stop_id: {type: mongoose.Types.ObjectId, ref: 'TripStop', required: true},
     seat_number: { type: Number, required: true },
-    purchase_date: { type: Date, default: Date.now }
+    purchase_date: { type: Date, default: Date.now },
+    status: { type: String, enum: ['ACTIVE', 'CANCELLED']},
 }, { timestamps: true })
 
 export const Ticket = mongoose.model('Ticket', ticketSchema)
