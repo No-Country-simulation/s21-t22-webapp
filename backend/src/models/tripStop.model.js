e40e2fec1374447a6b4dde16c44d784aaf25cd33
ezequielastrada <eastrada.in@gmail.com>
2025-02-24 20:54:05 -0300

    tripStop.model

---
+++ backend/src/models/tripStop.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const TripStopSchema = new mongoose.Schema({
+    trip_id: { type: mongoose.Schema.Types.ObjectId, ref: "Trip", required: true},
+    destination_id: { type: mongoose.Schema.Types.ObjectId, ref: "Destination", required: true},
+    stop_order: { type: Number, required: true},
+    arrival_time: { type: Date },
+    departure_time: { type: Date },
+    }, {timestamp: true}
+);
+
+export const TripStop = mongoose.model('TripStop', TripStopSchema)
