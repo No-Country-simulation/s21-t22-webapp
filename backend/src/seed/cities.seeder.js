dfc993edb911d3212ddf59d9fb44f3f0df827413
Federico González <alambratore@hotmail.com>
2025-02-25 18:09:24 -0300

    Improve Cities seeder

--- backend/src/seed/cities.seeder.js
+++ backend/src/seed/cities.seeder.js
@@ -1,5 +1,34 @@
+import { ObjectId } from 'mongodb';
+
 export const cities = [
-  { name: 'Buenos Aires', country: 'Argentina', population: 1 },
-  { name: 'Córdoba', country: 'Argentina', population: 1 },
-  { name: 'Corrientes', country: 'Argentina', population: 1 },
+  {
+    _id: new ObjectId('60d5ec49f1a4c2d2f8f0b1a1'),
+    name: 'Buenos Aires - Retiro',
+    country: 'Argentina',
+    population: 1,
+  },
+  {
+    _id: new ObjectId('60d5ec49f1a4c2d2f8f0b1a2'),
+    name: 'Buenos Aires - Liniers',
+    country: 'Argentina',
+    population: 1,
+  },
+  {
+    _id: new ObjectId('60d5ec49f1a4c2d2f8f0b1a3'),
+    name: 'Rosario',
+    country: 'Argentina',
+    population: 1,
+  },
+  {
+    _id: new ObjectId('60d5ec49f1a4c2d2f8f0b1a4'),
+    name: 'Villa María',
+    country: 'Argentina',
+    population: 1,
+  },
+  {
+    _id: new ObjectId('60d5ec49f1a4c2d2f8f0b1a5'),
+    name: 'Córdoba - Terminal de Ómnibus',
+    country: 'Argentina',
+    population: 1,
+  },
 ];
