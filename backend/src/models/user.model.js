6593887cfc9f61ed697d26ee5aac41b1d541f116
ezequielastrada <eastrada.in@gmail.com>
2025-02-25 20:43:35 -0300

    refactor user.model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,21 +4,9 @@ const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: [true, "Please add your name"] },
     lastname: { type: String, required: [true, "Please add your lastname"] },
-    phone: {
-      type: String,
-      required: [true, "Please add your phone"],
-      unique: true,
-    },
-    dni: {
-      type: String,
-      required: [true, "Please add your dni"],
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: [true, "Please add your email"],
-      unique: true,
-    },
+    phone: { type: String, required: [true, "Please add your phone"], unique: true },
+    dni: { type: String, required: [true, "Please add your dni"], unique: true },
+    email: { type: String, required: [true, "Please add your email"], unique: true },
     password: { type: String, required: [true, "Please add your password"] },
     role: { type: String, enum: ["USER", "ADMIN"], default: "USER" },
     miles_balace: { type: Number, default: 0}
