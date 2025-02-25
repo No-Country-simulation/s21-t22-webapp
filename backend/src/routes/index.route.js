e44eb5c3cc239a2d74b9009b8eb3652a7452812c
ezequielastrada <eastrada.in@gmail.com>
2025-02-25 20:49:39 -0300

    add use: /user, /ticket, /destination, /bus, /company, /trip, /tripstop

--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -1,10 +1,20 @@
 import express from "express";
 import userRouter from "./user.route.js";
-import cityRouter from "./cities.route.js";
+import ticketRouter from "./ticket.route.js";
+import destinationRouter from "./destination.route.js";
+import busRouter from "./bus.route.js"
+import companyRouter from "./company.route.js"
+import tripRouter from "./trip.route.js"
+import tripStopRouter from "./tripStop.route.js"
 
 const indexRouter = express.Router(); // 🔹 Corregido
 
 indexRouter.use("/user", userRouter);
-indexRouter.use("/cities", cityRouter);
+indexRouter.use("/ticket", ticketRouter);
+indexRouter.use("/destination", destinationRouter);
+indexRouter.use("/bus", busRouter);
+indexRouter.use("/company", companyRouter);
+indexRouter.use("/trip", tripRouter);
+indexRouter.use("/tripstop", tripStopRouter)
 
 export default indexRouter;
