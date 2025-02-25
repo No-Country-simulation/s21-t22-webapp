536400c48356bae1eea5ef57c6e4207d387718a0
ezequielastrada <eastrada.in@gmail.com>
2025-02-25 20:47:27 -0300

    delete cities.route

--- backend/src/routes/cities.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import cityController from "../controllers/city.controller.js";
-
-const cityRouter = express.Router();
-
-cityRouter.get("/search", cityController.searchCities);
-
-export default cityRouter;
