d8321d8f36046bf87158c23f28ae9a5691980d32
ezequielastrada <eastrada.in@gmail.com>
2025-02-25 20:40:53 -0300

    rename

--- backend/src/models/distination.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-export const destinationSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true },
-    city: { type: String, required: true },
-    code: { type: String, unique: true, required: true }
-}, { timestamps: true });
-
-export const Destination = mongoose.model('Destination', destinationSchema);
