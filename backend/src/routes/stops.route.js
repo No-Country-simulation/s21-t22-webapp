6b11f4a96afd34cb9e69c81af9e06994ed07126f
ezequielastrada <eastrada.in@gmail.com>
2025-02-28 06:25:51 -0300

    add /searchtrip, /search

--- backend/src/routes/stops.route.js
+++ backend/src/routes/stops.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { createStop } from "../controllers/stops/create.controller.js";
+import { buscarViajePorFecha, buscarStopsPorQuery } from "../controllers/stop.controller.js";
 
 const stopsRouter = express.Router();
 
 stopsRouter.post("/create", createStop);
+stopsRouter.get("/searchtrip", buscarViajePorFecha)
+stopsRouter.get("/search", buscarStopsPorQuery)
 
 export default stopsRouter;
