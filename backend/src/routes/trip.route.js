275b2d59dd2d2e95d5f7f9c80e9beecd461e407c
ezequielastrada <eastrada.in@gmail.com>
2025-02-28 15:58:56 -0300

    trip.route buscarViajePorFecha

--- backend/src/routes/trip.route.js
+++ backend/src/routes/trip.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createTripController, getTripController, searchTripsController } from "../controllers/trip.controller.js";
+import { 
+    createTripController, 
+    getTripController, 
+    searchTripsController, 
+    buscarViajePorFecha 
+} from "../controllers/trip.controller.js";
 
 const tripRouter = express.Router(); // Asegúrate de definir esto antes de usarlo
 
 tripRouter.post("/create", createTripController);
 tripRouter.get("/get/:routeId", getTripController);
 tripRouter.get("/search", searchTripsController);
+tripRouter.get("/search-trips", buscarViajePorFecha)
 
 export default tripRouter;
