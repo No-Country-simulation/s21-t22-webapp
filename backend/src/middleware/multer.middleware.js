3addb6ac9181913935bdd167b7da3a5c0e46ce9e
ezequielastrada <eastrada.in@gmail.com>
2025-03-07 23:21:30 -0300

    middleware multer

---
+++ backend/src/middleware/multer.middleware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage(); // Guarda en RAM en lugar de disco
+
+
+const fileFilter = (req, file, cb) => {
+
+  if (file.mimetype.startsWith("image/")) {
+    
+    cb(null, true);
+
+  } else {
+
+    cb(new Error("Solo se permiten imágenes"), false);
+
+  }
+};
+
+export const upload = multer({ storage, fileFilter, limits: { fileSize: 5 * 1024 * 1024 } }); // Máx 5MB
