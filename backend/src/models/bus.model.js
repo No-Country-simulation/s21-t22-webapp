c596884847804b61cef68e15706ef51a5e6228ad
ezequielastrada <eastrada.in@gmail.com>
2025-03-09 17:53:22 -0300

    bus.model: fix company

--- backend/src/models/bus.model.js
+++ backend/src/models/bus.model.js
@@ -4,8 +4,9 @@ const BusSchema = new mongoose.Schema(
   {
     plate: { type: String, required: true, unique: true }, // Placa del bus
     capacity: { type: Number, required: true }, // Número total de asientos
+    company: { type: String, required: true }
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 export default mongoose.model("Bus", BusSchema);
