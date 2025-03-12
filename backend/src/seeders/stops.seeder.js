e2e9c2293602528f68a5f753365202337452ba79
ezequielastrada <eastrada.in@gmail.com>
2025-03-12 13:47:55 -0300

    stops.seeder

--- backend/src/seeders/stops.seeder.js
+++ backend/src/seeders/stops.seeder.js
@@ -89,4 +89,184 @@ export const stops = [
     },
     imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/sanjuan.jpg"
   },
+  {
+    name: "San Salvador de Jujuy",
+    city: "Jujuy",
+    location: {
+      lat: -24.1858,
+      lng: -65.2995,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/jujuy.jpg"
+  },
+  {
+    name: "Villa Carlos Paz",
+    city: "Córdoba",
+    location: {
+      lat: -31.4241,
+      lng: -64.4978,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/villa_carlos_paz.jpg"
+  },
+  {
+    name: "Concordia",
+    city: "Entre Ríos",
+    location: {
+      lat: -31.3931,
+      lng: -58.0174,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/concordia.jpg"
+  },
+  {
+    name: "San Rafael",
+    city: "Mendoza",
+    location: {
+      lat: -34.6121,
+      lng: -68.3285,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/san_rafael.jpg"
+  },
+  {
+    name: "Goya",
+    city: "Corrientes",
+    location: {
+      lat: -29.1395,
+      lng: -59.2655,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/goya.jpg"
+  },
+  {
+    name: "San Fernando del Valle de Catamarca",
+    city: "Catamarca",
+    location: {
+      lat: -28.4696,
+      lng: -65.7852,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/catamarca.jpg"
+  },
+  {
+    name: "General Roca",
+    city: "Río Negro",
+    location: {
+      lat: -39.0333,
+      lng: -67.5833,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/general_roca.jpg"
+  },
+  {
+    name: "Tandil",
+    city: "Buenos Aires",
+    location: {
+      lat: -37.3217,
+      lng: -59.1332,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/tandil.jpg"
+  },
+  {
+    name: "Esquel",
+    city: "Chubut",
+    location: {
+      lat: -42.9138,
+      lng: -71.3199,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/esquel.jpg"
+  },
+  {
+    name: "Mercedes",
+    city: "Buenos Aires",
+    location: {
+      lat: -34.6511,
+      lng: -59.4309,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/mercedes.jpg"
+  },
+  {
+    name: "Cafayate",
+    city: "Salta",
+    location: {
+      lat: -26.0721,
+      lng: -65.9761,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/cafayate.jpg"
+  },
+  {
+    name: "Venado Tuerto",
+    city: "Santa Fe",
+    location: {
+      lat: -33.7456,
+      lng: -61.9688,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/venado_tuerto.jpg"
+  },
+  {
+    name: "Chivilcoy",
+    city: "Buenos Aires",
+    location: {
+      lat: -34.8951,
+      lng: -60.0166,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/chivilcoy.jpg"
+  },
+  {
+    name: "Azul",
+    city: "Buenos Aires",
+    location: {
+      lat: -36.7751,
+      lng: -59.8582,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/azul.jpg"
+  },
+  {
+    name: "Zapala",
+    city: "Neuquén",
+    location: {
+      lat: -38.8996,
+      lng: -70.0675,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/zapala.jpg"
+  },
+  {
+    name: "Pergamino",
+    city: "Buenos Aires",
+    location: {
+      lat: -33.8935,
+      lng: -60.5736,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/pergamino.jpg"
+  },
+  {
+    name: "Villa Mercedes",
+    city: "San Luis",
+    location: {
+      lat: -33.6757,
+      lng: -65.4578,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/villa_mercedes.jpg"
+  },
+  {
+    name: "Cutral Có",
+    city: "Neuquén",
+    location: {
+      lat: -38.9399,
+      lng: -69.2264,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/cutral_co.jpg"
+  },
+  {
+    name: "Clorinda",
+    city: "Formosa",
+    location: {
+      lat: -25.2842,
+      lng: -57.7181,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/clorinda.jpg"
+  },
+  {
+    name: "Puerto San Julián",
+    city: "Santa Cruz",
+    location: {
+      lat: -49.3064,
+      lng: -67.7286,
+    },
+    imgUrl: "https://culture-ui-document.s3.us-east-2.amazonaws.com/s21-22/puerto_san_julian.jpg"
+  }
 ];
